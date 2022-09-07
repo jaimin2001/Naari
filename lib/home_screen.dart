11c78c4c20c34dc7316054f04fce8418a652bb4d
jaimin2001 <jaiminmevada2001@gmail.com>
2022-09-07 09:25:23 +0530

    Women Saftey App

---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:naari/widgets/customappBar.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  //const HomeScreen({Key? key});
+  int qIndex = 0;
+
+  getRandomQuote() {
+    Random random = Random();
+
+    setState(() {
+      qIndex = random.nextInt(6);
+    });
+  }
+
+  @override
+  void initState() {
+    getRandomQuote();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              CustomAppBar(
+                  quoteIndex: qIndex,
+                  onTap: () {
+                    getRandomQuote();
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/quotes.dart
@@ -0,0 +1,8 @@
+List sweetSayings = [
+  "Your presence, lights up the whole room",
+  "We admire,Your strong personality.",
+  "Well help you In any way we can.",
+  "You are Strong and courageous",
+  'I say if Im beautiful. I say if Im strong',
+  'Above all, be the heroine of your life, not the victim',
+];
